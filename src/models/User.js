1dd244a67b98ec2525fe8dd1ac9375081efd7ee9
acones96 <71656477+acones96@users.noreply.github.com>
2020-09-24 12:56:52 -0500

    Add files via upload

--- src/models/User.js
+++ src/models/User.js
@@ -1,29 +1,21 @@
-const {Schema, model}=require("mongoose");
+const { Schema, model } = require ("mongoose");
 const bcrypt = require("bcryptjs");
+
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: Script, required: true },
 }, {
     timestamps: true
-});
+}); 
 
-UserSchema.methods.encrypPass = async password => {
+UserSchema.methods.encrypPassword = async password => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
 };
 
-UserSchema.methods.matchPass = function(password) => {
+UserSchema.method.matchPassword = function(password) {
     return await bcrypt.compare(password, this.password)
-}
+};
 
-module.exports = model("User", UserSchema);
+module.exports = model("User", UserSchema)
